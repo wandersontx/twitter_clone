66d55e88dcb8d300b9cd1f9fb46b29681845c465
wandersontx <wandersonh89@gmail.com>
2020-07-25 12:37:17 -0300

    Autenticação de usuarios

--- App/Connection.php
+++ App/Connection.php
@@ -6,10 +6,11 @@ class Connection{
     public static function getDB(){
         try{
            $conn = new \PDO(
-            "mysql:host=localhost;dbname=mvc;charset=utf8",
+            "mysql:host=localhost;dbname=twitter_clone;charset=utf8",
             "root",
-            ""
-        );
+            "" );
+            return $conn;
+
         }
         catch(\PDOException $e){
             //tratar erro
